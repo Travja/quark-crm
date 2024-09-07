93c926321740bde23c4573d2298a482c5aa6649e
Travja <the.only.t.craft@gmail.com>
2024-09-07 12:20:18 -0600

    Create a default .env if one doesn't exist pulling the variables straight from the system

--- scripts/env-replace.js
+++ scripts/env-replace.js
@@ -3,6 +3,12 @@ const path = require('path');
 require('dotenv').config();
 
 const envFile = path.resolve(__dirname, '../.env');
+if (!fs.existsSync(envFile)) {
+  console.log('.env file not found. Exiting.');
+  // Write VITE_API_URL to a new .env file so we have something to pull from.
+  // There may be more required environment variables in the future.
+  fs.writeFileSync(envFile, `VITE_API_URL=${process.env.VITE_API_URL}\n`);
+}
 const config = require('dotenv').parse(fs.readFileSync(envFile));
 
 const distDir = path.resolve(__dirname, '../dist'); // Adjust if your output directory is different
